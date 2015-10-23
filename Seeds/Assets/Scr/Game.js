4f77a3289e25ff96a145992a3b51c20a38a94714
William Terry <Tezza48is@aol.com>
2015-10-23 17:23:33 +0100

    Binary Tree Generator

---
+++ Seeds/Assets/Scr/Game.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+public class Game extends MonoBehaviour
+{
+  @Range(5 ,15)
+  public var mazeWidth : int;
+  @Range(5, 15)
+  public var mazeHeight : int;
+
+  private var MazeBinaryTree : MazeBinaryTree;
+
+  function Start () {
+    MazeBinaryTree.GenerateMaze(mazeWidth, mazeHeight);
+  }
+
+  function Update () {
+
+  }
+}
