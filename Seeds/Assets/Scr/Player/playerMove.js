772e2ece1f36d80be3f537c05d3e55da8ba7ee12
William Terry <Tezza48is@aol.com>
2016-01-18 20:10:30 +0000

    new movement

--- Seeds/Assets/Scr/Player/playerMove.js
+++ Seeds/Assets/Scr/Player/playerMove.js
@@ -1,18 +1,44 @@
 ﻿#pragma strict
 
 private var rigid : Rigidbody;
-private var speed : float = 10f;
+private var walkForce : float = 10f;
+private var maxSpeed : float = 10f;
+private var jumpForce : float = 10f;
+private var jumpCheckOrigin : Transform;
 
 function Start () {
 	rigid = GetComponent.<Rigidbody>();
+	jumpCheckOrigin = transform.Find("JumpCheck");
 }
 
-function Update () {
-	
+function FixedUpdate () {
+
 	var xInput = Input.GetAxisRaw("Horizontal");
-	var yInput = Input.GetAxis("Vertical");
-	
-	var targetDirection = (transform.forward * yInput + transform.right * xInput) * speed;
-	
-	rigid.velocity = targetDirection;
+	var yInput = Input.GetAxisRaw("Vertical");
+
+	var targetDirection = (transform.forward * yInput + transform.right * xInput) * walkForce;
+
+	rigid.AddForce(targetDirection);
+	if (rigid.velocity.magnitude > maxSpeed){
+		rigid.AddForce(-rigid.velocity);
+	}
+
+	Jump();
+}
+
+function Jump (){
+	var isGrounded = GroundedCheck();
+	if(isGrounded && Input.GetButton("Jump")){
+		rigid.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+	}
+}
+
+function GroundedCheck () : boolean{
+	var ray : RaycastHit;
+	if (Physics.Raycast(jumpCheckOrigin.position, -Vector3.up, 0.1)){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
