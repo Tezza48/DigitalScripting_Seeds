1e2b7f93073adeb626c0a76d8a8f858a843bfef7
William Terry <Tezza48is@aol.com>
2016-01-20 20:17:35 +0000

    separate shaders for terminal bits and starting a new seed input method

--- Seeds/Assets/Scr/Player/EnterComputerTrigger.js
+++ Seeds/Assets/Scr/Player/EnterComputerTrigger.js
@@ -1,29 +1,22 @@
 ﻿#pragma strict
 
-public var computerMenu : UnityEngine.UI.Image;
+public var computerPrompt : UnityEngine.UI.Text;
 
 function Start () {
 	// find the terminal ui menu and disable it
-	computerMenu = GameObject.FindGameObjectWithTag("Terminal Menu").GetComponent.<UnityEngine.UI.Image>();
-	computerMenu.gameObject.SetActive(false);
+	computerPrompt.gameObject.SetActive(false);
 }
 
 function OnTriggerEnter (other : Collider) {
 	if (other.tag == "Terminal"){
-		Cursor.lockState = CursorLockMode.None;
-		Cursor.visible = true;
-		GetComponent.<PlayerLook>().cameraLocked = true;
 		// activate the terminal ui
-		computerMenu.gameObject.SetActive(true);
+		computerPrompt.gameObject.SetActive(true);
 	}
 }
 
 function OnTriggerExit (other : Collider) {
 	if (other.tag == "Terminal"){
-		Cursor.lockState = CursorLockMode.Locked;
-		Cursor.visible = false;
-		GetComponent.<PlayerLook>().cameraLocked = false;
 		// deactivate the terminal ui
-		computerMenu.gameObject.SetActive(false);
+		computerPrompt.gameObject.SetActive(false);
 	}
 }
