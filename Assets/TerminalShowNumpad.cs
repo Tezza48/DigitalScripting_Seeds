b657cf3d89cffc50958878ff6c08784fb5c44691
William Terry <Tezza48is@aol.com>
2017-05-13 16:26:46 +0100

    Starting to implement new Terminal interface

---
+++ Assets/TerminalShowNumpad.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TerminalShowNumpad : MonoBehaviour {
+
+    public GameObject mDisplayedObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            ShowNumpad();
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            HideNumpad();
+        }
+    }
+
+    private void HideNumpad()
+    {
+        mDisplayedObject.SetActive(false);
+    }
+
+    private void ShowNumpad()
+    {
+        mDisplayedObject.SetActive(false);
+    }
+
+
+}
